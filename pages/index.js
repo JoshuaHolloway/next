884480074baa801b97ccf427451cf204b2934f81
JoshuaHolloway <jhollow6@asu.edu>
2021-03-26 18:26:03 -0500

    init

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,27 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Josh Title</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="JOSH" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by clicking our contact page:
+          <Link href="/contact">
+            <a>Click here!!!!</a>
+          </Link>
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
